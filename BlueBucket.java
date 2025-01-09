d5dc203ce278f1ae396aed2d7ac85d4eac1f6317
LakshmiKataru <lnkataru21@gmail.com>
2025-01-08 22:44:11 -0500

    Update BlueBucket.java
     Updated code

--- BlueBucket.java
+++ BlueBucket.java
@@ -12,13 +12,84 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Autonomous
 public class BlueBucket extends LinearOpMode {
-
+    private DcMotor liftMotor;
+    private Servo armServo;
+    private Servo clawRotationServo;
+    private Servo claw1;
+    private Servo claw2;
+    
     @Override
     public void runOpMode() {
         Pose2d startPose = new Pose2d(15, 64, Math.toRadians(270));
         MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
         Mechanisms mech = new Mechanisms(this, hardwareMap);
+        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        clawRotationServo = hardwareMap.get(Servo.class "clawRotation");
+         claw1 = hardwareMap.get(Servo.class, "claw1");
+        claw2 = hardwareMap.get(Servo.class, "claw2");
+        
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        waitForStart();
+        if (isStopRequested()) return;
+        // Actions
 
+        Action moveToLiftPosition(int position){
+            return new Action(){
+
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet){
+                if(!initialized){
+
+                    liftMotor.setTargetPosition(position);
+                    liftMotor.setPower(0.7);
+                    initialized = true;
+                }
+                    packet.put("Lift Position", liftMotor.getCurrentPosition());
+                    return liftMotor.isBusy();
+                
+            }
+       };
+  }
+        Action moveArmAndClaw(double armPosition, double clawRotationPosition) {
+            return new Action(){
+                private boolean initialized = false;
+                @Override
+                public bollean run(@NonNull TelemetryPacket packet){
+                    if(!initialized) { 
+                        armServo.setPosition(armPosition);
+                        clawRotationServo.setPosition(clawRotationPosition);
+                        initialized = true;
+                    }
+                    packet.put("Arm Position", armServo.getPosition());
+                    packet.put("Claw Rotation Position", clawRotationServo.getPosition());
+                    return false;
+                }
+                
+            };
+
+        }
+    Action setClawPosition(double position1, double position2){
+        return new Action(){
+            private boolean initialized = false;
+            @Overrode
+            public boolean run(@NonNull TelemetryPacket packet){
+                if (!initialized){
+                    claw1.setPosition(position1);
+                    claw2.setPosition(position2);
+                    initialized = true;
+                }
+                packet.put("Claw1 Position", claw1.getPosition());
+                packet.put("Claw2 Position", claw2.getPosition());
+                return false;
+            }
+            
+        };
+    
+    }
         //scores preload
         Action preload = drive.actionBuilder(startPose)
                 .splineToLinearHeading(new Pose2d(54,54, Math.toRadians(45)), Math.toRadians(0)) //score preload
@@ -67,20 +138,31 @@ public class BlueBucket extends LinearOpMode {
         Actions.runBlocking(
                 new SequentialAction(
                         preload,
-                        //score it
+                        moveToLiftPosition(-2000),
+                        moveArmAndClaw(0.5, 0.3),
+                        setClawPosition(0.65 ,0.35), //Open claw to release preloaded sample
                         grab2,
-                        //grab sample closest to sub
+                        moveToLiftPosition(-350)
+                        setClawPosition(0.4, 0.2
                         score2,
-                        //score it
+                        moveToLiftPosition(-2000),
+                        setClawPosition(0.0, 0.8), // Open claw to release
                         pickup3,
-                        //grab second sample from sub
+                        setClawPosition(0.8, 0.1
                         score3,
-                        //score it
+                        moveToLiftPosition(-2000),
+                        setClawPosition(0.0, 0.8),
                         pickup4,
-                        //grab furthest sample
+                        moveToLiftPosition(-350),
+                        setClawPosition(0.4, 0.2);
                         score4,
-                        //score it
-                        park
+                        moveToLiftPosition(-2000),
+                        setClawPosition(0.0, 0.8), // Open claw to release            
+                        park,
+                        () -> {
+                            telemetry.addData("Status", "Parked");
+                            telemetry.update();
+                        }
                 )
         );
     }
